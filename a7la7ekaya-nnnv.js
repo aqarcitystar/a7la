e39a65a87df73293bdd924a2ad7fa2d92c96d50a
ahmedto <ahmed.to2004@gmail.com>
2021-02-06 17:57:33 +0200

    Add files via upload

---
+++ a7la7ekaya-nnnv.js
@@ -0,0 +1,39 @@
+﻿function showaccountinfo() {
+
+	if (document.getElementById('a7lasettingsmenu7ekaya').style.display == 'block') { $('#a7lasettingsmenu7ekaya').addClass('animated fadeOutDown');$('#a7lasettingsmenu7ekaya').fadeOut(200) }
+	if (document.getElementById('accountinfo').style.display == 'block') { $('#accountinfo').addClass('animated fadeOutDown');$('#accountinfo').fadeOut(200) }
+	else if (document.getElementById('accountinfo').style.display == 'none') { $('#accountinfo').addClass('animated pulse');$('#accountinfo').fadeIn(200) }
+
+
+}
+
+function a7lasettingsmenu7ekaya() {
+
+	if (document.getElementById('accountinfo').style.display == 'block') { $('#accountinfo').fadeOut(200) }
+	if (document.getElementById('a7lasettingsmenu7ekaya').style.display == 'block') { $('#a7lasettingsmenu7ekaya').fadeOut(200) }
+	else if (document.getElementById('a7lasettingsmenu7ekaya').style.display == 'none') { $('#a7lasettingsmenu7ekaya').addClass('animated fadeInLeft');$('#a7lasettingsmenu7ekaya').fadeIn(200) }
+}
+
+
+function showapages() {
+
+	if (document.getElementById('a7lasettingsmenu7ekaya').style.display == 'block') { $('#a7lasettingsmenu7ekaya').addClass('animated fadeOutDown');$('#a7lasettingsmenu7ekaya').fadeOut(200) }
+	if (document.getElementById('accountinfo').style.display == 'block') { $('#accountinfo').addClass('animated fadeOutDown');$('#accountinfo').fadeOut(200) }
+	else if (document.getElementById('pages-contents').style.display == 'none') { $('#pages-contents').addClass('animated pulse');$('#pages-contents').fadeIn(200) }
+}
+
+
+function dropdown(element) {
+$(element).addClass('animated flipInX');$(element).fadeIn(500);
+}
+
+$(document).mouseup(function (e)
+{
+    var container = $(".dropdown");
+
+    if (!container.is(e.target) // if the target of the click isn't the container...
+        && container.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+        container.fadeOut(200);
+    }
+});
---
+++ a7la7ekaya-ooov.js
@@ -0,0 +1,49 @@
+﻿
+function display() {
+
+	if (document.cookie.indexOf("tpbr") >= 0) {
+	document.getElementById('tbds').checked = 'checked';
+	$(".bar").css('position','absolute');
+	$("#appmenu").css('position','absolute');
+	$("#accountinfo").css('position','absolute');
+	$("#a7lasettingsmenu7ekaya").css('position','absolute');
+	$(".notif1").css('position','absolute');
+	}
+
+	if (document.cookie.indexOf("sdbr") <= 0) {
+	$(".sidebar").fadeIn(500);
+	$("#homebody").css('margin-left','250px');
+	}
+	else { document.getElementById('sbds').checked = 'checked'; }
+}
+
+function tpbrcck() {
+	if (document.cookie.indexOf("tpbr") >= 0) {
+	document.cookie="tpbr=cck; expires=Thu, 18 Dec 1970 12:00:00 GMT";
+	}
+	else {
+	document.cookie="tpbr=cck; expires=Thu, 18 Dec 2070 12:00:00 GMT";
+	}
+}
+
+function sdbrcck() {
+	if (document.cookie.indexOf("sdbr") >= 0) {
+	document.cookie="sdbr=cck; expires=Thu, 18 Dec 1970 12:00:00 GMT";
+	}
+	else {
+	document.cookie="sdbr=cck; expires=Thu, 18 Dec 2070 12:00:00 GMT";
+	}
+}
+
+
+$(document).mouseup(function (e)
+{
+    var container = $(".menus");
+
+    if (!container.is(e.target)
+        && container.has(e.target).length === 0)
+    {
+        container.hide();
+    }
+});
+
